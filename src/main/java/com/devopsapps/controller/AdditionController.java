3e1dd40ffe1b9a468f3d9094b09d14ac8de2be6e
nizamanwar <nanwar@tillster.com>
2021-11-05 16:32:09 +0530

    first app to test devops culture

---
+++ src/main/java/com/devopsapps/controller/AdditionController.java
@@ -0,0 +1,26 @@
+package com.devopsapps.controller;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class AdditionController {
+
+	private static int sum = 0;
+	
+	@RequestMapping("add")
+	public ModelAndView add(HttpServletRequest request, HttpServletResponse response) {
+		int num1 = Integer.parseInt(request.getParameter("num1"));
+		int num2 = Integer.parseInt(request.getParameter("num2"));
+		sum = num1 + num2;
+		
+		ModelAndView mv = new ModelAndView();
+		mv.addObject("result", sum);
+		mv.setViewName("result");
+		return mv;
+	}
+}
---
+++ src/main/webapp/index.jsp
@@ -0,0 +1,10 @@
+<html>
+<body>
+<h2>Calculator</h2>
+<form action = "add">
+Number1: <input type = "number" name = "num1"></input><br/>
+Number2: <input type = "number" name = "num2"></input><br/>
+<button type = "submit">Add</button>
+</form>
+</body>
+</html>
