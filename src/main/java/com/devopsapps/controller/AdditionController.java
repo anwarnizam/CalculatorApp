889da1eddf9b740695e417ff74a888404b1b4da6
nizamanwar <nanwar@tillster.com>
2021-11-08 15:01:58 +0530

    Fix isuue type vulnerability generated by SonarQUbe

--- src/main/java/com/devopsapps/controller/AdditionController.java
+++ src/main/java/com/devopsapps/controller/AdditionController.java
@@ -5,16 +5,21 @@ import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class AdditionController {
 	
-	@RequestMapping("add")
+	//private static int sum = 0;
+	
+	//@RequestMapping("add")
+	//Adding method parameter to fix SonarQube issue type - Vulnerability
+	@RequestMapping(value = "add", method = RequestMethod.POST)
 	public ModelAndView add(HttpServletRequest request, HttpServletResponse response) {
 		int num1 = Integer.parseInt(request.getParameter("num1"));
 		int num2 = Integer.parseInt(request.getParameter("num2"));
-		//made local to fix sonarqube issue code smell, earlier was global with private static
+		//Made local to fix SonarQube issue type - Code Smell
 		int sum = num1 + num2;
 		
 		ModelAndView mv = new ModelAndView();
